f61c140d6e8b2bc79ff27b6259725b5516d028ea
Julio Muller <juliodolago@gmail.com>
2022-04-30 00:37:29 -0300

    feat: define routes, models and seeders for MirageJS

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,63 @@
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 export function initializeMirageServer() {
   return createServer({
-    routes() {
-      this.namespace = 'api'
+    models: {
+      transaction: Model,
+    },
 
-      this.get('/transactions', () => {
-        return [
+    seeds(server) {
+      server.db.loadData({
+        transactions: [
           {
             id: 1,
             title: 'Salário abr/2022',
             category: 'Remuneração',
             date: '07/04/2022',
             amount: 5400,
+            type: 'income',
           },
           {
             id: 2,
             title: 'Almoço',
             category: 'Alimentação',
             date: '07/04/2022',
-            amount: -38,
+            amount: 38,
+            type: 'expense',
           },
           {
             id: 3,
             title: 'Eletricidade 04/2022',
             category: 'Contas fixas',
             date: '10/04/2022',
-            amount: -280,
+            amount: 280,
+            type: 'expense',
           },
-        ]
+        ],
+      })
+    },
+
+    routes() {
+      this.namespace = 'api'
+
+      this.get('/transactions', (schema) => {
+        return schema.all('transaction')
+      })
+
+      this.post('/transactions', (schema, request) => {
+        const requestBody = JSON.parse(request.requestBody)
+        const newTransaction: any = {
+          title: requestBody.title,
+          category: requestBody.category,
+          amount: requestBody.amount,
+          type: requestBody.type,
+          date: new Date().toISOString(),
+          id: Math.floor(Math.random() * 1000),
+        }
+
+        schema.create('transaction', newTransaction)
+
+        return newTransaction
       })
     },
   })
--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -4,12 +4,13 @@ const baseUrl = '/api/transactions'
 
 async function getTransactions() {
   const response = await fetch(baseUrl)
-  const transactions = await response.json()
+  const responseJson = await response.json()
+  const { transactions } = responseJson
 
   return transactions as Transaction[]
 }
 
-async function createTransaction(transaction: Omit<Transaction, 'id'>) {
+async function createTransaction(transaction: Omit<Transaction, 'id' | 'date'>) {
   const response = await fetch(baseUrl, {
     method: 'POST',
     headers: {
